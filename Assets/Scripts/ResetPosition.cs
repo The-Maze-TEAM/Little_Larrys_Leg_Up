81bac8f6993ac9bbeb24dac6988d5db7ffab42c5
magicaldave <smokydave@icloud.com>
2023-03-10 04:39:40 -0600

    Edit: Finally figured out a new solution to resetting the player position using Physics.SyncTransforms(). Added some documentation and a link to a forum post with benchmarks also.

--- Assets/Scripts/ResetPosition.cs
+++ Assets/Scripts/ResetPosition.cs
@@ -16,9 +16,25 @@ public class ResetPosition : MonoBehaviour
 	{
 		if (other.name == "Player")
 		{
-			control.enabled = false;
-			other.transform.position = new Vector3(0, 30, 0);
-			control.enabled = true;
+			// This method works, but is slightly more expensive.
+			// However, for scenarios where the framerate is significantly
+			// higher than the physics step, this method may be more reliable.
+			// It seems to call Physics.SyncTransforms under the hood.
+			    // control.enabled = false;
+			    // other.transform.position = _teleportDestination;
+			    // control.enabled = true;
+			        // Alternatively, you can do Edit > Project Settings > Physics > AutoSyncTransforms = True
+			        // Or set Physics.autoSyncTransforms to True via script.
+			// We could also just ditch CharacterControllers altogether over Rigidbody physics,
+			// but I'm not in favor of that idea.
+
+			// Benchmarks from a similar scenario seem to indicate this is the easiest
+			// and most performant option for this project.
+
+			// https://forum.unity.com/threads/character-controller-ignores-transform-position.617107/
+
+			other.transform.position = _teleportDestination;
+			Physics.SyncTransforms();
 		}
 		else
 			Destroy(other.gameObject);
